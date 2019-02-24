b63e1fb59cf0775d8f3c2b8ecccd050c8078737f
Shirley Keter <shirleyketer@gmail.com>
2019-02-25 00:06:32 +0300

    add brackets in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-
+// Business (or back-end) logic:
 $(document).ready(function() {
  $("form#myForm").submit(function(event) {
    event.preventDefault();
 
-var Q1 =parseInt($("input:radio[name=answer1]:checked")).val();
-var Q2 =parseInt($("input:radio[name=answer2]:checked")).val();
-var Q3 =parseInt($("input:radio[name=answer3]:checked")).val();
+var Q1 =parseInt($("input:radio[name=answer1]:checked").val());
+var Q2 =parseInt($("input:radio[name=answer2]:checked").val());
+var Q3 =parseInt($("input:radio[name=answer3]:checked").val());
 $("#yourscore").text(Q1+Q2+Q3);
-  )};
+)};
 )};
