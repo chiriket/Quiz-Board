0bfdc1ca78485af10401d4a1f1145193f1eef605
Shirley Keter <shirleyketer@gmail.com>
2019-02-25 11:42:18 +0300

    adding qstns html

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 var Q1 =parseInt($("input:radio[name=answer1]:checked").val());
 var Q2 =parseInt($("input:radio[name=answer2]:checked").val());
 var Q3 =parseInt($("input:radio[name=answer3]:checked").val());
-$("#output").text(Q1+Q2+Q3)/40*100;
+var Q4 =parseInt($("input:radio[name=answer4]:checked").val());
+$("#output").text(Q1+Q2+Q3+Q4)/40*100;
 });
 });
