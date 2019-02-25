dac1bba23fd94566ed257429164caef57ff7e845
Shirley Keter <shirleyketer@gmail.com>
2019-02-25 06:21:11 +0300

    h1changes

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,6 @@ $(document).ready(function() {
 var Q1 =parseInt($("input:radio[name=answer1]:checked").val());
 var Q2 =parseInt($("input:radio[name=answer2]:checked").val());
 var Q3 =parseInt($("input:radio[name=answer3]:checked").val());
-$("#yourscore").text(Q1+Q2+Q3);
-)};
-)};
+$("#output").text(Q1+Q2+Q3);
+});
+});
