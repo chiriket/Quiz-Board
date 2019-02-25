06c83d4205b73f0cb50381ad342afb5322878c5f
Shirley Keter <shirleyketer@gmail.com>
2019-02-25 10:05:53 +0300

    add qstn html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business (or back-end) logic:
+// UI logic:
 $(document).ready(function() {
  $("form#myForm").submit(function(event) {
    event.preventDefault();
@@ -6,6 +6,6 @@ $(document).ready(function() {
 var Q1 =parseInt($("input:radio[name=answer1]:checked").val());
 var Q2 =parseInt($("input:radio[name=answer2]:checked").val());
 var Q3 =parseInt($("input:radio[name=answer3]:checked").val());
-$("#output").text(Q1+Q2+Q3);
+$("#output").text(Q1+Q2+Q3)/40*100;
 });
 });
